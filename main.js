3666dd35104aaa90e687f38e6cd34f9fa8e91f6e
TutusBOT <barte77pl@gmail.com>
2022-02-07 02:08:30 +0100

    getting user id

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
-getData()
+getUserId()
 
 
-async function getData(){
+async function getUserId(){
     const response = await fetch("https://eun1.api.riotgames.com/lol/summoner/v4/summoners/by-name/TutusBOT?api_key=RGAPI-29a95ea5-86a5-426d-8b6b-327429bd9b59")
+    const results = await response.json()
+    const basicInfo = await getData(results.id)
+    console.log(results);
+}
+
+async function getData(userId){
+    console.log(userId);
+    const response = await fetch("https://eun1.api.riotgames.com/lol/league/v4/entries/by-summoner/"+userId+"?api_key=RGAPI-29a95ea5-86a5-426d-8b6b-327429bd9b59")
     const results = await response.json()
     console.log(results);
 }
