23274588fe7ebaf3935431e5f1bfaf4e7b9b455e
TutusBOT <barte77pl@gmail.com>
2022-02-07 11:35:50 +0100

    get matched by puuid

--- main.js
+++ main.js
@@ -1,17 +1,27 @@
-var summoner = document.getElementById('summonerName').value
+
 
 
 async function getUserId(){
-    const response = await fetch("https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/"+summoner+"?api_key=RGAPI-2b678016-90bb-4aa0-9f6b-6b90d55ef73b")
+    let summoner = document.getElementById('summonerName').value
+    const response = await fetch("https://eun1.api.riotgames.com/lol/summoner/v4/summoners/by-name/"+summoner+"?api_key=RGAPI-29a95ea5-86a5-426d-8b6b-327429bd9b59")
     const results = await response.json()
+    
     const basicInfo = await getData(results.id)
+    
+    const matchList = await getMatchId(results.puuid)
+    console.log(matchList.json());
     console.log(results);
     document.getElementById('wynik').innerHTML = results.summonerLevel;
 }
 
 async function getData(userId){
     console.log(userId);
-    const response = await fetch("https://euw1.api.riotgames.com/lol/league/v4/entries/by-summoner/"+userId+"?api_key=RGAPI-2b678016-90bb-4aa0-9f6b-6b90d55ef73b")
+    const response = await fetch("https://eun1.api.riotgames.com/lol/league/v4/entries/by-summoner/"+userId+"?api_key=RGAPI-29a95ea5-86a5-426d-8b6b-327429bd9b59")
     const results = await response.json()
     console.log(results);
+}
+
+async function getMatchId(puuid){
+    const response = await fetch("https://europe.api.riotgames.com/lol/match/v5/matches/by-puuid/"+puuid+"/ids?start=0&count=20&api_key=RGAPI-29a95ea5-86a5-426d-8b6b-327429bd9b59")
+    return response
 }
