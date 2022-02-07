9c4ce856349b28a8a98e9de68010c8a300c71cbf
TutusBOT <barte77pl@gmail.com>
2022-02-07 14:56:43 +0100

    match data

--- main.js
+++ main.js
@@ -9,9 +9,12 @@ async function getUserId(){
     const basicInfo = await getData(results.id)
     
     const matchList = await getMatchId(results.puuid)
+
+    const matchData = await getMatchData(matchList[0])
     console.log("list of matches id", matchList);
     console.log("user id", results);
     console.log("basic info",basicInfo);
+    console.log("match data", matchData);
     document.getElementById('wynik').innerHTML = results.summonerLevel;
 }
 
@@ -23,5 +26,12 @@ async function getData(userId){
 
 async function getMatchId(puuid){
     const response = await fetch("https://europe.api.riotgames.com/lol/match/v5/matches/by-puuid/"+puuid+"/ids?start=0&count=20&api_key=RGAPI-29a95ea5-86a5-426d-8b6b-327429bd9b59")
-    return response.json()
+    const results = await response.json()
+    return results
+}
+
+async function getMatchData(matchId){
+    const response = await fetch("https://europe.api.riotgames.com/lol/match/v5/matches/"+matchId+"?api_key=RGAPI-29a95ea5-86a5-426d-8b6b-327429bd9b59")
+    const results = await response.json()
+    return results
 }
