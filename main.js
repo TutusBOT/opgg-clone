61ba2ff9ecfdc49b67315f54568e4bbb90b73d3d
TutusBOT <barte77pl@gmail.com>
2022-02-07 01:38:49 +0100

    cos

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ getData()
 
 
 async function getData(){
-    const response = await fetch("https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/TutusBOT?api_key=RGAPI-2c255ea8-75fa-44d3-90e0-175da48fe731")
+    const response = await fetch("https://eun1.api.riotgames.com/lol/summoner/v4/summoners/by-name/TutusBOT?api_key=RGAPI-29a95ea5-86a5-426d-8b6b-327429bd9b59")
     const results = await response.json()
     console.log(results);
 }
