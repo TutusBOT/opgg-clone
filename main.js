2cf3849335fb034c745dafca2cb3c26e13575d3b
“Jacek” <Jacek.spalek@gmail.com>
2022-02-07 02:11:15 +0100

    siema

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
+
+
+
 getUserId()
-
-
 async function getUserId(){
     const response = await fetch("https://eun1.api.riotgames.com/lol/summoner/v4/summoners/by-name/TutusBOT?api_key=RGAPI-29a95ea5-86a5-426d-8b6b-327429bd9b59")
     const results = await response.json()
